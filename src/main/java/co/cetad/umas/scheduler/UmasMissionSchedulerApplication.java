76fe04ff00e652c7675338c25d208d8f54df4da2
Cristian Osia <osia.developer@gmail.com>
2025-12-28 15:09:16 -0500

    Init

---
+++ src/main/java/co/cetad/umas/scheduler/UmasMissionSchedulerApplication.java
@@ -0,0 +1,13 @@
+package co.cetad.umas.scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmasMissionSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmasMissionSchedulerApplication.class, args);
+	}
+
+}
